f2c87ef816f0476e83e3fa8489f4040dea295d54
Annie <malatay.ana@outlook.com>
2025-03-11 21:51:44 +0000

    Initial commit

---
+++ streetcare-backend/src/main/java/app_reporting_api/StreetcareBackendApplication.java
@@ -0,0 +1,13 @@
+package app_reporting_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetcareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetcareBackendApplication.class, args);
+	}
+
+}
---
+++ streetcare-backend/src/test/java/app_reporting_api/StreetcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app_reporting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetcareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
